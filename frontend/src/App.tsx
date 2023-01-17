a90c308c702417f6c33646739400833d746844a3
ak2dev <akrambakr73@gmail.com>
2023-01-17 16:45:31 +0400

    login component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,16 @@ import {
 } from "react-router-dom";
 import './App.css';
 import ProductsPage from "./pages/ProductsPage";
-
+import Login from "./pages/login";
+import Nav from "./pages/Nav";
 function App() {
   return (
     <div className="App">
       <Router>
+          <Nav />
         <Routes>
           <Route path={"products"} element={<ProductsPage />} />
+          <Route path={"login"} element={<Login />} />
         </Routes>
       </Router>
     </div>
